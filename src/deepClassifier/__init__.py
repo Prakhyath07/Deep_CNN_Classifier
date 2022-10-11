5cc074a8908f03a6719dd2c721fbcd099ee28654
Prakhyath Bhandary <prakhz16@gmail.com>
2022-10-11 19:20:25 +0530

    dataingestion trial

--- src/deepClassifier/__init__.py
+++ src/deepClassifier/__init__.py
@@ -3,13 +3,13 @@ import sys
 import logging
 from tkinter.tix import Tree
 
-logging_str = "[%(asctime)s: %(levlename)s: %(module)s: %(message)s"
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s"
 log_dir = "logs"
 log_filepath = os.path.join(log_dir,"running_logs.log")
 os.makedirs(log_dir,exist_ok=True)
 
 logging.basicConfig(
-    level=logging.info,
+    level=logging.INFO,
     format = logging_str,
     handlers= [logging.StreamHandler(sys.stdout),
     logging.FileHandler(log_filepath)]
